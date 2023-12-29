0ea0566d4fe6111e2afdb7f34619919ec6a61f1e
Webifi <john@webifi.com>
2023-12-28 22:48:09 -0600

    Add ComfyUI-Manager

--- 05.sh
+++ 05.sh
@@ -26,6 +26,13 @@ if [ ! -d ${SD05_DIR}/ComfyUI ]; then
     git clone https://github.com/comfyanonymous/ComfyUI ${SD05_DIR}/ComfyUI
 fi
 
+if [ ! -d ${SD05_DIR}/ComfyUI/custom_nodes/ComfyUI-Manager ]; then
+    git clone https://github.com/ltdrdata/ComfyUI-Manager.git ${SD05_DIR}/ComfyUI/custom_nodes/ComfyUI-Manager
+fi
+
+cd ${SD05_DIR}/ComfyUI/custom_nodes/ComfyUI-Manager
+git pull -X ours
+
 cd ${SD05_DIR}/ComfyUI
 git pull -X ours
 
@@ -34,9 +41,11 @@ sl_folder ${SD05_DIR}/ComfyUI/models checkpoints ${BASE_DIR}/models stable-diffu
 sl_folder ${SD05_DIR}/ComfyUI/models hypernetworks ${BASE_DIR}/models hypernetwork
 sl_folder ${SD05_DIR}/ComfyUI/models loras ${BASE_DIR}/models lora
 sl_folder ${SD05_DIR}/ComfyUI/models vae ${BASE_DIR}/models vae
+sl_folder ${SD05_DIR}/ComfyUI/models vae_approx ${BASE_DIR}/models vae_approx
 sl_folder ${SD05_DIR}/ComfyUI/models embeddings ${BASE_DIR}/models embeddings
 sl_folder ${SD05_DIR}/ComfyUI/models upscale_models ${BASE_DIR}/models upscale
 sl_folder ${SD05_DIR}/ComfyUI/models clip_vision ${BASE_DIR}/models clip_vision
+sl_folder ${SD05_DIR}/ComfyUI/models clip ${BASE_DIR}/models clip
 sl_folder ${SD05_DIR}/ComfyUI/models controlnet ${BASE_DIR}/models controlnet
 
 if [ -d ${SD05_DIR}/venv ]; then
