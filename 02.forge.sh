e6828fcc289717eb25b569722435e5c18ac5da68
Holaf <32452391+grokuku@users.noreply.github.com>
2024-02-25 12:01:01 +0100

    add forge fork + add use of common folders

--- 02.forge.sh
+++ 02.forge.sh
@@ -18,11 +18,11 @@ conda install -c conda-forge git python=3.11 pip gcc gxx libcurand --solver=libm
 
 
 
-if [ ! -d ${SD02_DIR}/webui ]; then
-    git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui ${SD02_DIR}/webui
+if [ ! -d ${SD02_DIR}/forge ]; then
+    git clone https://github.com/lllyasviel/stable-diffusion-webui-forge.git ${SD02_DIR}/forge
 fi
 
-cd ${SD02_DIR}/webui
+cd ${SD02_DIR}/forge
 
 if [ -d "${SD02_DIR}/python-venv" ]; then
     # check if remote is ahead of local
@@ -49,7 +49,7 @@ fi
 if [ ! -d ${SD02_DIR}/python-venv ]; then
     cd ${SD02_DIR}
     python -m venv python-venv
-    cd ${SD02_DIR}/webui
+    cd ${SD02_DIR}
     source python-venv/bin/activate
     pip install --upgrade pip
     pip install packaging
@@ -61,22 +61,22 @@ fi
 
 # Merge Models, vae, lora, and hypernetworks, and outputs
 # Ignore move errors if they occur
-sl_folder ${SD02_DIR}/webui/models Stable-diffusion ${BASE_DIR}/models stable-diffusion
-sl_folder ${SD02_DIR}/webui/models hypernetworks ${BASE_DIR}/models hypernetwork
-sl_folder ${SD02_DIR}/webui/models Lora ${BASE_DIR}/models lora
-sl_folder ${SD02_DIR}/webui/models VAE ${BASE_DIR}/models vae
-sl_folder ${SD02_DIR}/webui embeddings ${BASE_DIR}/models embeddings
-sl_folder ${SD02_DIR}/webui/models ESRGAN ${BASE_DIR}/models upscale
-sl_folder ${SD02_DIR}/webui/models BLIP ${BASE_DIR}/models blip
-sl_folder ${SD02_DIR}/webui/models Codeformer ${BASE_DIR}/models codeformer
-sl_folder ${SD02_DIR}/webui/models GFPGAN ${BASE_DIR}/models gfpgan
-sl_folder ${SD02_DIR}/webui/models LDSR ${BASE_DIR}/models ldsr
-sl_folder ${SD02_DIR}/webui/models ControlNet ${BASE_DIR}/models controlnet
+sl_folder ${SD02_DIR}/forge/models Stable-diffusion ${BASE_DIR}/models stable-diffusion
+sl_folder ${SD02_DIR}/forge/models hypernetworks ${BASE_DIR}/models hypernetwork
+sl_folder ${SD02_DIR}/forge/models Lora ${BASE_DIR}/models lora
+sl_folder ${SD02_DIR}/forge/models VAE ${BASE_DIR}/models vae
+sl_folder ${SD02_DIR}/forge embeddings ${BASE_DIR}/models embeddings
+sl_folder ${SD02_DIR}/forge/models ESRGAN ${BASE_DIR}/models upscale
+sl_folder ${SD02_DIR}/forge/models BLIP ${BASE_DIR}/models blip
+sl_folder ${SD02_DIR}/forge/models Codeformer ${BASE_DIR}/models codeformer
+sl_folder ${SD02_DIR}/forge/models GFPGAN ${BASE_DIR}/models gfpgan
+sl_folder ${SD02_DIR}/forge/models LDSR ${BASE_DIR}/models ldsr
+sl_folder ${SD02_DIR}/forge/models ControlNet ${BASE_DIR}/models controlnet
 
-sl_folder ${SD02_DIR}/webui outputs ${BASE_DIR}/outputs 02-sd-webui
+sl_folder ${SD02_DIR}/forge outputs ${BASE_DIR}/outputs 02-sd-webui
 
-echo "Run Stable-Diffusion-WebUI"
-cd ${SD02_DIR}/webui
+echo "Run Stable-Diffusion-WebUI-forge"
+cd ${SD02_DIR}
 source python-venv/bin/activate
 export PATH="/config/02-sd-webui/forge/python-venv/lib/python3.11/site-packages/onnxruntime/capi:$PATH"
 pip install --upgrade pip
