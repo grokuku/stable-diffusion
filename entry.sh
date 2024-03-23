0608ae9135fa8b4771d0db3f65fb871bbdddcac7
nico <nico@holaf.fr>
2024-03-23 11:32:22 +0100

    Change the way access rights are handled

--- entry.sh
+++ entry.sh
@@ -7,8 +7,21 @@ if [ ! -f "$BASE_DIR/scripts/custom-sample.sh" ]; then
     cp -v "/custom-sample.sh" "$BASE_DIR/scripts/custom-sample.sh"
 fi
 
+# reset rights if file "Delete this file to reset access rights" isn't present
+if [ ! -f "config/Delete this file to reset access rights" ]; then
+echo "chown'ing directory to ensure correct permissions."
+chown -R abc:users /config
+chmod -R 774 /config
+chmod -R 664 /config/models
+chmod -R 664 /config/outputs
+find /config -type d -exec chmod 777 {} +
+echo Delete this file to reset access rights > "/config/Delete this file to reset access rights"
+echo "Done!"
+fi
+
+
 . /$WEBUI_VERSION.sh
 . /$WEBUI_VERSION
 . $BASE_DIR/scripts/$WEBUI_VERSION
 . $BASE_DIR/scripts/$WEBUI_VERSION.sh 
-echo error in webui selection variable
+echo error when launching WebUI
