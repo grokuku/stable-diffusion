f10cca12b0adc66277bfcabea7e642f46cc86e07
nico <nico@holaf.fr>
2024-03-24 22:11:37 +0100

    new Kohya setup

--- 70.sh
+++ 70.sh
@@ -13,14 +13,14 @@ fi
 
 source activate ${SD70_DIR}/env
 conda install -n base conda-libmamba-solver -y
-conda install -c conda-forge git python=3.10 pip --solver=libmamba -y
+conda install -c conda-forge python=3.10 pip --solver=libmamba -y
 
 # Create venv
-if [ ! -d ${SD70_DIR}/venv ]; then
-    cd ${SD70_DIR}
-    python -m venv venv
-    cd ${SD70_DIR}
-fi
+#if [ ! -d ${SD70_DIR}/venv ]; then
+#    cd ${SD70_DIR}
+#    python -m venv venv
+#    cd ${SD70_DIR}
+#fi
 
 if [ ! -f "$SD70_DIR/parameters.txt" ]; then
   cp -v "${SD_INSTALL_DIR}/parameters/70.txt" "$SD70_DIR/parameters.txt"
@@ -31,20 +31,21 @@ if [ ! -d ${SD70_DIR}/kohya_ss ]; then
 fi
 
     cd ${SD70_DIR}/kohya_ss
-    git config --global --add safe.directory ${SD70_DIR}/kohya_ss
+#    git config --global --add safe.directory ${SD70_DIR}/kohya_ss
     git pull -X ours
 
 #    if [ ! -d ${SD70_DIR}/venv ]; then
 #      su -w SD70_DIR - diffusion -c 'cd ${SD70_DIR} && python3 -m venv venv'
 #    fi
 
-cd ${SD70_DIR}
-source venv/bin/activate
+#cd ${SD70_DIR}
+#source venv/bin/activate
 pip install --upgrade pip
 cd ${SD70_DIR}/kohya_ss
-pip install -r requirements.txt
+python ./setup/setup_linux.py
+#pip install -r requirements.txt
 cd ${SD70_DIR}/kohya_ss
-CMD="bash gui.sh"; while IFS= read -r param; do if [[ $param != \#* ]]; then CMD+=" ${param}"; fi; done < "${SD70_DIR}/parameters.txt"; eval $CMD
+CMD="python kohya_gui.py"; while IFS= read -r param; do if [[ $param != \#* ]]; then CMD+=" ${param}"; fi; done < "${SD70_DIR}/parameters.txt"; eval $CMD
 
 
 
