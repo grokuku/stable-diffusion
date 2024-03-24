a720346fc90ecd2f04e6be98fe84d9d7e97f1c9b
nico <nico@holaf.fr>
2024-03-24 14:23:30 +0100

    update 07 and 08

--- 07.sh
+++ 07.sh
@@ -7,29 +7,59 @@ export SD07_DIR=${BASE_DIR}/07-StableSwarm
 mkdir -p ${SD07_DIR}
 mkdir -p /config/outputs/07-StableSwarm
 
-if [ ! -d ${SD07_DIR}/env ]; then
-    conda create -p ${SD07_DIR}/env -y
-fi
-
-source activate ${SD07_DIR}/env
-conda install -n base conda-libmamba-solver -y
-conda install -c conda-forge git python=3.11 pip --solver=libmamba -y
-
+#remove old venv if still exists
 if [ -d ${SD07_DIR}/venv ]; then
     rm -rf ${SD07_DIR}/venv
 fi
 
+#copy default parameters if missing
 if [ ! -f "$SD07_DIR/parameters.txt" ]; then
     cp -v "${SD_INSTALL_DIR}/parameters/07.txt" "$SD07_DIR/parameters.txt"
 fi
 
+#clone repository if new install
 if [ ! -d ${SD07_DIR}/StableSwarmUI ]; then
-    cd "${SD07_DIR}" && git clone https://github.com/Stability-AI/StableSwarmUI
+    cd "${SD07_DIR}" && git clone https://github.com/Stability-AI/StableSwarmUI.git
 fi
 
+# check if remote is ahead of local
+# https://stackoverflow.com/a/25109122/1469797
 cd ${SD07_DIR}/StableSwarmUI
-git pull -X ours
+if [ "$CLEAN_ENV" != "true" ] && [ $(git rev-parse HEAD) = $(git ls-remote $(git rev-parse --abbrev-ref @{u} | \
+sed 's/\// /g') | cut -f1) ]; then
+    echo "Local branch up-to-date, keeping existing venv"
+    else
+        if [ "$CLEAN_ENV" = "true" ]; then
+        echo "Forced wiping venv for clean packages install"
+        else
+        echo "Remote branch is ahead. Wiping venv for clean packages install"
+        fi
+    export active_clean=1
+#    git reset --hard HEAD
+    git pull -X ours
+fi
 
+#clean conda env if needed
+if [ "$active_clean" = "1" ]; then
+    echo "-------------------------------------"
+    echo "Cleaning venv"
+    rm -rf ${SD07_DIR}/env
+    export active_clean=0
+    echo "Done!"
+    echo -e "-------------------------------------\n"
+fi
+
+#create env if missing
+if [ ! -d ${SD07_DIR}/env ]; then
+    conda create -p ${SD07_DIR}/env -y
+fi
+
+#activate env and install packages
+source activate ${SD07_DIR}/env
+conda install -n base conda-libmamba-solver -y
+conda install -c conda-forge git python=3.11 pip --solver=libmamba -y
+
+#move models to common folder and create symlinks
 mkdir -p ${SD07_DIR}/StableSwarmUI/Models
 
 sl_folder ${SD07_DIR}/StableSwarmUI/Models Stable-Diffusion ${BASE_DIR}/models stable-diffusion
@@ -41,6 +71,7 @@ sl_folder ${SD07_DIR}/StableSwarmUI/Models controlnet ${BASE_DIR}/models control
 
 sl_folder ${SD07_DIR}/StableSwarmUI Output ${BASE_DIR}/outputs 07-StableSwarm
 
+#launch Stable Swarm
 cd ${SD07_DIR}/StableSwarmUI
 CMD="./launch-linux.sh"
 while IFS= read -r param; do
--- 08.sh
+++ 08.sh
@@ -7,28 +7,54 @@ export SD08_DIR=${BASE_DIR}/08-voltaML
 mkdir -p ${SD08_DIR}
 mkdir -p /config/outputs/08-voltaML
 
-if [ ! -d ${SD08_DIR}/env ]; then
-    conda create -p ${SD08_DIR}/env -y
-fi
-
-source activate ${SD08_DIR}/env
-conda install -n base conda-libmamba-solver -y
-conda install -c conda-forge git python=3.10 pip=22.3.1 gcc gxx --solver=libmamba -y
-conda install pytorch torchvision torchaudio -c pytorch --solver=libmamba -y
-
-
-
 if [ ! -f "$SD08_DIR/parameters.txt" ]; then
     cp -v "${SD_INSTALL_DIR}/parameters/08.txt" "$SD08_DIR/parameters.txt"
 fi
 
 if [ ! -d "${SD08_DIR}/voltaML-fast-stable-diffusion" ]; then
-    cd "${SD08_DIR}" && git clone https://github.com/VoltaML/voltaML-fast-stable-diffusion
+    cd "${SD08_DIR}" && git clone https://github.com/VoltaML/voltaML-fast-stable-diffusion.git
 fi
 
+# check if remote is ahead of local
+# https://stackoverflow.com/a/25109122/1469797
 cd ${SD08_DIR}/voltaML-fast-stable-diffusion
-git pull -X ours
+if [ "$CLEAN_ENV" != "true" ] && [ $(git rev-parse HEAD) = $(git ls-remote $(git rev-parse --abbrev-ref @{u} | \
+sed 's/\// /g') | cut -f1) ]; then
+    echo "Local branch up-to-date, keeping existing venv"
+    else
+        if [ "$CLEAN_ENV" = "true" ]; then
+        echo "Forced wiping venv for clean packages install"
+        else
+        echo "Remote branch is ahead. Wiping venv for clean packages install"
+        fi
+    export active_clean=1
+#    git reset --hard HEAD
+    git pull -X ours
+fi
 
+#clean conda env if needed
+if [ "$active_clean" = "1" ]; then
+    echo "-------------------------------------"
+    echo "Cleaning venv"
+    rm -rf ${SD08_DIR}/env
+    rm -rf ${SD08_DIR}/voltaML-fast-stable-diffusion/venv
+    export active_clean=0
+    echo "Done!"
+    echo -e "-------------------------------------\n"
+fi
+
+#create conda env if missing
+if [ ! -d ${SD08_DIR}/env ]; then
+    conda create -p ${SD08_DIR}/env -y
+fi
+
+#activate conda env and install packages
+source activate ${SD08_DIR}/env
+conda install -n base conda-libmamba-solver -y
+conda install -c conda-forge git python=3.10 pip=22.3.1 gcc gxx --solver=libmamba -y
+conda install pytorch torchvision torchaudio -c pytorch --solver=libmamba -y
+
+#create python venv if missing
 if [ ! -d ${SD08_DIR}/voltaML-fast-stable-diffusion/venv ]; then
     cd ${SD08_DIR}/voltaML-fast-stable-diffusion
     python -m venv venv
@@ -44,6 +70,7 @@ for fichier in ${SD08_DIR}/voltaML-fast-stable-diffusion/requirements/*.txt; do
     pip install -r $fichier
 done
 
+#move models to common folders and create symlinks
 sl_folder ${SD08_DIR}/voltaML-fast-stable-diffusion/data models ${BASE_DIR}/models stable-diffusion
 sl_folder ${SD08_DIR}/voltaML-fast-stable-diffusion/data lora ${BASE_DIR}/models lora
 sl_folder ${SD08_DIR}/voltaML-fast-stable-diffusion/data vae ${BASE_DIR}/models vae
@@ -51,16 +78,8 @@ sl_folder ${SD08_DIR}/voltaML-fast-stable-diffusion/data textual-inversion ${BAS
 sl_folder ${SD08_DIR}/voltaML-fast-stable-diffusion/data upscaler ${BASE_DIR}/models upscale
 sl_folder ${SD08_DIR}/voltaML-fast-stable-diffusion/data outputs /config/outputs 08-voltaML
 
-# cd ${SD08_DIR}
-# conda install -c conda-forge pytorch torchvis --solver=libmamba -y
-# pip install -v -U git+https://github.com/chengzeyi/stable-fast.git@main#egg=stable-fast
-# su -w SD08_DIR - diffusion -c 'cd ${SD08_DIR} && source venv/bin/activate && pip install torch==2.1.0+cu118 torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118 '
-# su -w SD08_DIR - diffusion -c 'cd ${SD08_DIR} && source venv/bin/activate && pip install diffusers xformers ninja '
-# su -w SD08_DIR - diffusion -c 'cd ${SD08_DIR} && source venv/bin/activate && pip install -v -U git+https://github.com/chengzeyi/stable-fast.git@main#egg=stable-fast  '
-
-# Boucle pour parcourir tous les fichiers requirements.txt
 cd ${SD08_DIR}/voltaML-fast-stable-diffusion
-# source venv/bin/activate
+# launch Volta ML
 CMD="python3 main.py"
 while IFS= read -r param; do
     if [[ $param != \#* ]]; then
